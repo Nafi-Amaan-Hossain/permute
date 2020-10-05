1a5a7d94f10e237ed6caee27d6159a1fdeae4077
Nafi-Amaan-Hossain <68556185+Nafi-Amaan-Hossain@users.noreply.github.com>
2020-10-05 14:17:46 +0530

    Update permutation.py

--- permutation.py
+++ permutation.py
@@ -4,7 +4,7 @@ import sys
 
 def cracker(passarg):
     starttime = time.time()
-    chars = ['#','@','$']
+    chars = ['#','@','$','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z','0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
     password_buffer = ""
 
     for x in string.ascii_letters:
